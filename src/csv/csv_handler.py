c4392ee67816407d729e683b003ce88ab7bbe8e2
Dominic Roy-Stang <dominicroystang@gmail.com>
2018-11-08 21:12:20 -0500

    Added full handling for mint-formatted data. Most of the functionality for preformatted format is done as well.

--- src/csv/csv_handler.py
+++ src/csv/csv_handler.py
@@ -18,33 +18,96 @@ def determine_format(csv_file):
     current_format = first_row.columns.tolist()
 
     # Search for a corresponding supported format
-    if (current_format == csv_formats['mint']):
+    if current_format == csv_formats['preformatted']:
+        return 'preformatted'
+    if current_format == csv_formats['mint']:
         return 'mint'
     else:
         raise Exception('CSV format not supported!')
 
 
+def handle_preformatted(csv_file):
+    """
+    Converts csv file in preformatted format to a dataframe
+    Returns the csv as a Pandas DataFrame
+    """
+    print('preformatted')
+    # Load from dataset
+    data = pandas.read_csv(csv_file)
+
+    # Set date format and sort by date
+    data['Date'] = pandas.to_datetime(data.Date)
+    data.sort_values(by='Date', inplace=True)
+
+    # Create and initialize index column
+    data.insert(1, 'Index', range(1, len(data)+1))
+    data.set_index('Index', inplace=True)
+    return data
+
+
 def handle_mint(csv_file):
-    print('MINT')
+    """
+    Formats and edits a dataset from a csv file in Mint format
+    Returns a Pandas DataFrame that
+        1. Has two columns: date, net worth
+        2. Is sorted by date (YYYY-MM-DD)
+    """
+    # Load relevant data from dataset
+    relevant_features = ['Date', 'Transaction Type', 'Amount']
+    data = pandas.read_csv(csv_file, usecols=relevant_features)
+
+    # Set date format and sort by date
+    data['Date'] = pandas.to_datetime(data.Date)
+    data.sort_values(by='Date', inplace=True)
+
+    # Create and initialize index column
+    data.insert(1, 'Index', range(1, len(data)+1))
+    data.set_index('Index', inplace=True)
+
+    # Add a net worth column with default value of 0
+    data.insert(1, 'Net Worth', 0)
+
+    # Calculate net worth
+    net_worth = 0
+    for index, row in data.iterrows():
+        transaction_type = row['Transaction Type']
+        amount = row['Amount']
+        if row['Transaction Type'] == 'credit':
+            net_worth += amount
+            data.loc[index, 'Net Worth'] = net_worth
+        elif row['Transaction Type'] == 'debit':
+            net_worth -= amount
+            data.loc[index, 'Net Worth'] = net_worth
+
+    # Remove features that are no longer needed
+    data.drop(["Amount", "Transaction Type"], axis=1, inplace=True)
+    print(data.columns.tolist())
+    print(data.values)
 
 
 def format_dataset(csv_file):
     """
-    Format and edit dataset such that it returns a Pandas DataFrame that
-    1. It has two columns: date, net worth
-    2. It is sorted by date (YYYY-MM-DD)
+    Format and edit dataset from a csv file.
+    Returns a Pandas DataFrame that
+        1. It has two columns: date, net worth
+        2. It is sorted by date (YYYY-MM-DD)
     """
+    # Determine csv format
     try:
         csv_format = determine_format(csv_file)
     except Exception as e:
         raise
 
-    if csv_format is 'mint':
+    # Handle the format
+    if csv_format is 'preformatted':
+        return handle_preformatted(csv_file)
+    elif csv_format is 'mint':
         return handle_mint(csv_file)
     else:
         raise Exception('CSV format unrecognized!')
 
 
 datasets_directory = Path.cwd()/"../../datasets"
-transactions_file = datasets_directory/"transactions.csv"
+# transactions_file = datasets_directory/"transactions.csv"
+transactions_file = datasets_directory/"test_preformatted.csv"
 format_dataset(transactions_file)
