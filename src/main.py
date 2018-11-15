e12d7e0d3ffa49614e6aec0a8c954b80557476bd
Dominic Roy-Stang <dominicroystang@gmail.com>
2018-11-14 22:28:43 -0500

    Added Makefile.

--- src/main.py
+++ src/main.py
@@ -11,7 +11,8 @@ project_root = Path(__file__).resolve().parent.parent
 
 def main():
     transactions_file = project_root/"datasets/transactions.csv"
-    csv_handler.format_dataset(transactions_file)
+    data = csv_handler.format_dataset(transactions_file)
+    print(data)
 
 
 if __name__ == "__main__":
